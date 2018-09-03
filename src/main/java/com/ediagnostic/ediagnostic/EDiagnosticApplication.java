4cb99df276403d3152c7bcf634da6f434018649a
diriarte <diego_iriarte@mcafee.com>
2018-09-03 18:51:01 -0300

    Initial commit

---
+++ src/main/java/com/ediagnostic/ediagnostic/EDiagnosticApplication.java
@@ -0,0 +1,12 @@
+package com.ediagnostic.ediagnostic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDiagnosticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiagnosticApplication.class, args);
+	}
+}
