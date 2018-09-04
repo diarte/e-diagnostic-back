37d4a9a9ba279cd8b2b4136702db1019563c4230
diriarte <diego_iriarte@mcafee.com>
2018-09-04 20:20:40 -0300

    add config

---
+++ src/main/java/com/ediagnostic/controller/EDiagnosticController.java
@@ -0,0 +1,15 @@
+package com.ediagnostic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class EDiagnosticController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+}
--- src/main/java/com/ediagnostic/ediagnostic/EDiagnosticApplication.java
+++ src/main/java/com/ediagnostic/ediagnostic/EDiagnosticApplication.java
@@ -1,9 +1,15 @@
 package com.ediagnostic.ediagnostic;
 
+import com.ediagnostic.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({SecurityConfig.class})
+@ComponentScan(value = "com.ediagnostic")
 public class EDiagnosticApplication {
 
 	public static void main(String[] args) {
